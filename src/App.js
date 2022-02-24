96df2d81dc2bfbd134bd195c091eb8dc321e0faa
Ellen Harris <ellenharris@Ellens-MacBook-Air.local>
2022-02-24 16:09:02 +0000

    updated state property of recipe when button clicked

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import "./App.css";
+import './App.css';
+import { GenerateRecipeButton } from './components/generate_recipe';
 
 function App() {
   return (
@@ -6,6 +7,22 @@ function App() {
       <header className="App-header">
         <h1>Recipe Generator</h1>
       </header>
+      <div>
+        <p>
+          Here's a recipe for some pasta. Don't like it? Click the button to
+          randomly generate a new recipe. For example it might be a...
+        </p>
+        <ul>
+          <li>Pie</li>
+          <li>Trifle</li>
+          <li>Curry</li>
+        </ul>
+        <p>
+          We hope you like experimental flavours, since the ingredients are
+          randomly generated as well..! &#128521;
+        </p>
+        <GenerateRecipeButton />
+      </div>
     </div>
   );
 }
--- src/components/generate_recipe.js
+++ src/components/generate_recipe.js
@@ -1,3 +1,61 @@
-function generateRecipeButton() {
-  return <button id="generate_recipe_button">Generate Recipe</button>;
+import { useState } from 'react';
+
+export function GenerateRecipeButton() {
+  const ingredients = [
+    'potato',
+    'apple',
+    'chocolate',
+    'cheese',
+    'carrot',
+    'raspberry',
+    'ham',
+  ];
+  const recipes = ['pie', 'curry', 'trifle', 'pasta', 'bagel'];
+
+  const [suggestedRecipe, setRecipe] = useState('pasta');
+
+  const [suggestedMethod, setMethod] = useState('method 1');
+
+  return (
+    <div>
+      <button id="generate_recipe_button" onClick={() => setRecipe('curry')}>
+        Generate New Recipe
+      </button>
+      <h2>Your recipe: A ham and cheese {suggestedRecipe}</h2>
+      {/* <h2>
+        Your recipe: A {<RandomElementSelector ingredients={ingredients} />} and{' '}
+        <br />
+        {<RandomElementSelector ingredients={ingredients} />} <br />
+        {<RandomElementSelector ingredients={recipes} />}
+      </h2> */}
+      <h2>Method</h2>
+      <CreateMethod suggestedRecipe={suggestedRecipe} />
+    </div>
+  );
+}
+
+function RandomElementSelector({ ingredients }) {
+  const length = ingredients.length;
+  return ingredients[Math.floor(Math.random() * length + 1)];
+}
+
+function CreateMethod(suggestedRecipe) {
+  if ({ suggestedRecipe } == 'curry') {
+    return (
+      <ol>
+        <li>method1</li>
+      </ol>
+    );
+  } else if ({ suggestedRecipe } == 'pasta') {
+    return (
+      <ol>
+        <li>method2</li>
+      </ol>
+    );
+  }
+  return (
+    <ol>
+      <li>default method</li>
+    </ol>
+  );
 }
